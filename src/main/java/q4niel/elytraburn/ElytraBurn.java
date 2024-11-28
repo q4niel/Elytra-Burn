8fa5be582ae4db2ab95d86454c81de734ccf4d43
q4niel <github.obscure938@passmail.net>
2024-11-28 21:58:39 +0100

    Fabric Mod Template

---
+++ src/main/java/q4niel/elytraburn/ElytraBurn.java
@@ -0,0 +1,14 @@
+package q4niel.elytraburn;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElytraBurn implements ModInitializer {
+	public static final String MOD_ID = "elytraburn";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/q4niel/elytraburn/ElytraBurnDataGenerator.java
@@ -0,0 +1,9 @@
+package q4niel.elytraburn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElytraBurnDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
